90d0a3bde18f7ef86c54d2a571715f3277bb3546
Dhavidy <p266dd@gmail.com>
2025-04-17 02:31:44 +0900

    Installed and configured firebase.

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp, getApps } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+let firebaseInstance, database, storage;
+
+if (!getApps().length) {
+  firebaseInstance = initializeApp(firebaseConfig);
+  database = getFirestore(firebaseInstance);
+  storage = getStorage(firebaseInstance);
+}
+
+export { database, storage };
