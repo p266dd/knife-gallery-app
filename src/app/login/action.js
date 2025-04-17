a87174b0c57a925107b14d896c12ad3dc47031ad
Dhavidy <p266dd@gmail.com>
2025-04-17 21:09:11 +0900

    Created login page and form action.

---
+++ src/app/login/action.js
@@ -0,0 +1,73 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { compareSync } from "bcryptjs";
+
+import prisma from "@/data/prisma";
+import { createSession } from "@/utils/session";
+
+// * Validation schema.
+import { loginFormSchema } from "@/data/validation/login-form";
+
+export default async function LoginAction(state, formData) {
+  // * Reset state errors and message.
+  // * Set state data from formData.
+  state = { errors: [], message: "", data: Object.fromEntries(formData) };
+
+  const loginData = state.data;
+
+  // * Will hold valid data.
+  let validData;
+
+  try {
+    validData = await loginFormSchema.validate(loginData, {
+      abortEarly: false,
+    });
+  } catch (error) {
+    // * Catch validation errors.
+    if (error.name === "ValidationError") {
+      const fieldErrors = {};
+      for (const fieldError of error.inner) {
+        fieldErrors[fieldError.path] = fieldError.message;
+      }
+      return { ...state, errors: fieldErrors };
+    }
+
+    // * Log any other error.
+    console.log(error);
+  }
+
+  // * Fetch user by its email
+  const user = await prisma.user.findFirst({
+    where: { email: validData.email },
+  });
+
+  if (!user) return (state = { ...state, message: "User does not exist." });
+
+  // * Check if password is correct.
+  const passwordsMatch = compareSync(validData.password, user.password);
+
+  if (!passwordsMatch)
+    return (state = { ...state, message: "Incorrect email or password." });
+
+  // * Check if user is currently active.
+  if (!user.isActive)
+    return (state = {
+      ...state,
+      message: "User is not active. Please wait while we review.",
+    });
+
+  // * Create session for the user.
+  // session will be stored in a httpOnly cookie.
+  await createSession({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  });
+
+  // * Redirect to dashboard if role is admin.
+  if (user.role === "admin") return redirect("/dashboard");
+
+  return redirect("/");
+}
---
+++ src/app/login/page.jsx
@@ -0,0 +1,174 @@
+"use client";
+
+import Link from "next/link";
+import { AnimatePresence, motion } from "motion/react";
+import { useState, useActionState } from "react";
+import { LoaderCircle, Eye, EyeOff } from "lucide-react";
+
+import BrandIcon from "@/ui/brand-icon";
+
+import LoginAction from "./action";
+import Button from "@/ui/button";
+
+export default function LoginPage() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [state, action, pending] = useActionState(LoginAction, {
+    errors: [],
+    data: {},
+    message: "",
+  });
+
+  const formAnimation = {
+    hidden: {
+      opacity: 0,
+      x: -80,
+      transition: {
+        duration: 0.3,
+      },
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.3,
+        ease: "easeInOut",
+      },
+    },
+    exit: {
+      opacity: 0,
+      x: 80,
+    },
+  };
+
+  return (
+    <main className="flex min-h-screen overflow-hidden bg-slate-100">
+      <div className="w-full h-screen flex flex-col justify-center items-center">
+        <div className="w-11/12 max-w-96">
+          <AnimatePresence>
+            <motion.div
+              layout
+              key="form"
+              variants={formAnimation}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              className="w-full p-6 bg-white rounded-lg shadow-md"
+            >
+              <div className="my-6">
+                <BrandIcon />
+              </div>
+
+              <div className="mb-8">
+                <h1 className="text-2xl font-semibold mb-3">
+                  Sign in to your account.
+                </h1>
+                <h4 className="text-sm text-slate-500">
+                  Not a member?
+                  <Link
+                    href="/register"
+                    className="inline-block ml-2 text-blue-400 cursor-pointer hover:underline"
+                  >
+                    Create an account.
+                  </Link>
+                </h4>
+              </div>
+
+              <div>
+                <form
+                  action={action}
+                  className="text-sm text-slate-700 flex flex-col gap-3"
+                >
+                  {state && state.message && (
+                    <div className="px-3 py-2 text-xs font-semibold bg-blue-200 text-blue-900 rounded-xl">
+                      {state.message}
+                    </div>
+                  )}
+
+                  <div className="flex flex-col gap-2">
+                    <label htmlFor="email" className="text-sm">
+                      Email address
+                    </label>
+                    <input
+                      type="text"
+                      name="email"
+                      defaultValue={state?.data?.email || null}
+                      className={`rounded-lg border-1 px-3 py-2 text-sm ${
+                        state?.errors && state?.errors?.name
+                          ? "border-red-500"
+                          : "border-slate-300"
+                      }`}
+                    />
+                    {state?.errors && state?.errors?.email && (
+                      <span className="text-red-600 text-xs font-semibold">
+                        {state.errors.email}
+                      </span>
+                    )}
+                  </div>
+
+                  <div className="flex flex-col gap-2">
+                    <label htmlFor="password">Password</label>
+                    <div className="relative">
+                      <input
+                        type={showPassword ? "text" : "password"}
+                        name="password"
+                        defaultValue={state?.data?.password || null}
+                        className={`w-full rounded-lg border-1 px-3 py-2 text-sm ${
+                          state?.errors && state?.errors?.name
+                            ? "border-red-500"
+                            : "border-slate-300"
+                        }`}
+                      />
+                      <span
+                        className="absolute right-4 top-2 text-slate-400"
+                        onClick={() => setShowPassword(!showPassword)}
+                      >
+                        {showPassword ? (
+                          <Eye size={20} />
+                        ) : (
+                          <EyeOff size={20} />
+                        )}
+                      </span>
+                    </div>
+                    {state?.errors && state?.errors?.password && (
+                      <span className="text-red-600 text-xs font-semibold">
+                        {state.errors.password}
+                      </span>
+                    )}
+                  </div>
+
+                  <div className="flex justify-end px-4 mb-3">
+                    <a
+                      onClick={() => setCurrent("recover")}
+                      className="text-blue-400 cursor-pointer hover:underline"
+                    >
+                      Forgot password?
+                    </a>
+                  </div>
+
+                  <div className="flex flex-col">
+                    <Button
+                      type="submit"
+                      variant={pending ? "loading" : "primary"}
+                    >
+                      {pending ? (
+                        <span>
+                          <LoaderCircle
+                            className="inline-block animate-spin mr-2"
+                            size={20}
+                          />{" "}
+                          Signing in
+                        </span>
+                      ) : (
+                        "Sign in"
+                      )}
+                    </Button>
+                  </div>
+                </form>
+              </div>
+            </motion.div>
+          </AnimatePresence>
+        </div>
+      </div>
+    </main>
+  );
+}
