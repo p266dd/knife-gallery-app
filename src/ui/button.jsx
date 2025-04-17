bcc853d6f57e8704306435412920580922d14ae2
Dhavidy <p266dd@gmail.com>
2025-04-17 21:51:42 +0900

    Created Button component.

---
+++ src/ui/button.jsx
@@ -0,0 +1,20 @@
+export default function Button({
+  type = "button",
+  variant = "primary",
+  onClick = () => {},
+  children,
+}) {
+  const styles = {
+    primary:
+      "px-4 py-3 bg-slate-800 text-white active:bg-slate-900 cursor-pointer font-semibold rounded-lg",
+    flat: "rounded-lg px-4 py-3 cursor-pointer bg-slate-100 text-slate-500 border-1 border-slate-300",
+    loading:
+      "rounded-lg px-4 py-3 cursor-pointer bg-slate-300 text-slate-600 border-1 border-slate-500",
+  };
+
+  return (
+    <button type={type} onClick={onClick} className={`${styles[variant]}`}>
+      {children}
+    </button>
+  );
+}
