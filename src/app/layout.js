288cd4a8abb6dec2e9ac0104eb3d0d3ac5f4785e
Dhavidy <p266dd@gmail.com>
2025-04-17 19:00:14 +0900

    Changed default font to Open Sans.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,12 @@
+import { Open_Sans } from "next/font/google";
 import "./globals.css";
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  variable: "--font-open-sans",
+  display: "swap",
+  weight: ["300", "400", "600", "700"],
+});
 
 export const metadata = {
   title: "Knife Gallery App",
