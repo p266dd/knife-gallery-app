c17b319c5988eb995342292b7aef3ba5f38593ee
Dhavidy <p266dd@gmail.com>
2025-04-18 22:57:15 +0900

    Wrote the fetch order functions.

--- src/actions/fetch-orders.js
+++ src/actions/fetch-orders.js
@@ -1,17 +1,110 @@
+"use server";
+
+import prisma from "@/data/prisma";
+
+export async function fetchNewOrders({ page, itemsPerPage }) {
+  // * return only new orders, and shallow query.
+  const orders = await prisma.order.findMany({
+    where: {
+      isCompleted: false,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      client: {
+        select: {
+          businessName: true,
+        },
+      },
+    },
+  });
+
+  const startIndex = (page - 1) * itemsPerPage;
+  const paginatedData = orders.slice(startIndex, startIndex + itemsPerPage);
+  const totalCount = orders.length;
+  const totalPages = Math.ceil(totalCount / itemsPerPage);
+
+  return { data: paginatedData, totalCount, totalPages, currentPage: page };
+}
+
 export async function fetchOrders({
   searchQuery,
   page,
   itemsPerPage,
-  onlyNew,
+  full = false,
 }) {
-  await new Promise((resolve) => setTimeout(resolve, 500));
+  // * Initialize query configuration.
+  let config = {
+    where: {
+      isCompleted: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      client: {
+        select: {
+          businessName: true,
+        },
+      },
+    },
+  };
+
+  // * Add all necessary fields to the query if full is true.
+  if (full) {
+    config = {
+      ...config,
+      include: {
+        client: {
+          select: {
+            name: true,
+            businessName: true,
+          },
+        },
+        orderProduct: {
+          select: {
+            details: true,
+            request: true,
+            products: {
+              include: {
+                sizes: true,
+                brand: true,
+                media: true,
+                thumbnail: true,
+              },
+            },
+          },
+        },
+      },
+    };
+  }
+
+  // * Handle the search query if user is searching for something.
+  if (searchQuery) {
+    config = {
+      ...config,
+      where: {
+        isCompleted: true,
+        OR: [
+          {
+            code: { contains: searchQuery },
+          },
+          {
+            client: {
+              businessName: { contains: searchQuery },
+            },
+          },
+        ],
+      },
+    };
+  }
+
+  const orders = await prisma.order.findMany(config);
+
   const startIndex = (page - 1) * itemsPerPage;
-  const mockData = Array.from(
-    { length: 4 },
-    (_, i) => `Item ${i + 1} for query: ${searchQuery}`
-  );
-  const paginatedData = mockData.slice(startIndex, startIndex + itemsPerPage);
-  const totalCount = mockData.length;
+  const paginatedData = orders.slice(startIndex, startIndex + itemsPerPage);
+  const totalCount = orders.length;
   const totalPages = Math.ceil(totalCount / itemsPerPage);
 
   return { data: paginatedData, totalCount, totalPages, currentPage: page };
