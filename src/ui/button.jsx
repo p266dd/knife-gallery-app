20ed6f92a8c1ce6520bb547068d02359e3dc8916
Dhavidy <p266dd@gmail.com>
2025-04-18 11:05:43 +0900

    Added size and disabled options.

--- src/ui/button.jsx
+++ src/ui/button.jsx
@@ -3,19 +3,32 @@
 export default function Button({
   type = "button",
   variant = "primary",
+  size = "md",
   onClick = () => {},
+  disabled = false,
   children,
 }) {
   const styles = {
     primary:
-      "px-4 py-3 bg-slate-800 text-white active:bg-slate-900 cursor-pointer font-semibold rounded-lg",
-    flat: "rounded-lg px-4 py-3 cursor-pointer bg-slate-100 text-slate-500 border-1 border-slate-300",
+      "bg-slate-800 text-white active:bg-slate-900 cursor-pointer rounded-lg",
+    flat: "rounded-lg cursor-pointer bg-slate-100 text-slate-500 border-1 border-slate-300",
     loading:
-      "rounded-lg px-4 py-3 cursor-pointer bg-slate-300 text-slate-600 border-1 border-slate-500",
+      "rounded-lg cursor-pointer bg-slate-300 text-slate-600 border-1 border-slate-500",
+  };
+
+  const sizes = {
+    sm: "px-2 py-1 text-xs",
+    md: "px-3 py-2 text-sm",
+    lg: "px-4 py-3 font-semibold",
   };
 
   return (
-    <button type={type} onClick={onClick} className={`${styles[variant]}`}>
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      className={`${styles[variant]} ${sizes[size]}`}
+    >
       {children}
     </button>
   );
