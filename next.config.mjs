7362b3f7375c826d5252e5e397440fd4a9c8eee2
Dhavidy <p266dd@gmail.com>
2025-04-24 14:08:37 +0900

    Added config to allow remote image loading.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    domains: ["firebasestorage.googleapis.com"],
+  },
+};
 
 export default nextConfig;
