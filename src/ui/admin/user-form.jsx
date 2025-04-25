2ac8d92ec4dd7198903fe794bcb7d2d83a5f385b
Dhavidy <p266dd@gmail.com>
2025-04-25 19:51:10 +0900

    Changed to use actionState.

--- src/ui/admin/user-form.jsx
+++ src/ui/admin/user-form.jsx
@@ -1,89 +1,26 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useActionState } from "react";
 import { motion } from "motion/react";
 
 import addProduct from "@/actions/add-product";
 
 export default function UserForm({ user, edit = false }) {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [message, setMessage] = useState(null);
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    password: "",
-    role: "",
-    businessName: "",
-    businessCode: "",
-    isActive: "",
+  const [state, formAction, isPending] = useActionState(addProduct, {
+    error: null,
+    message: null,
   });
-
-  // * Add a new product to database or save existing one.
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    setLoading(true);
-    setError(null);
-    setMessage(null);
-
-    if (edit) {
-      updateUser(formData, user)
-        .then((res) => {
-          if (res.errors) {
-            setError(res.errors);
-            setLoading(false);
-            setMessage(null);
-            return;
-          }
-          setLoading(false);
-          setMessage(null);
-          setError(null);
-          router.push("/dashboard/users");
-        })
-        .catch((error) => {
-          setLoading(false);
-          setError([error.message]);
-          setMessage(null);
-        });
-      return;
-    }
-
-    addUser(formData)
-      .then((res) => {
-        if (res.errors) {
-          setError(res.errors);
-          setLoading(false);
-          setMessage(null);
-          return;
-        }
-        setLoading(false);
-        setMessage(null);
-        setError(null);
-        router.push("/dashboard/users");
-      })
-      .catch((error) => {
-        setLoading(false);
-        setError([error.message]);
-        setMessage(null);
-      });
-  };
 
   return (
     <div>
-      <form className="relative">
+      <form onSubmit={formAction} className="relative">
         <div className="mb-3 flex items-center gap-3">
           <div className="flex-grow flex items-center gap-3 px-3 py-2 bg-white border border-slate-300 rounded-xl">
             <input
               name="role"
               id="user-role"
               type="radio"
-              defaultChecked={formData.role === "user"}
-              disabled={edit && product.role === "user"}
-              onChange={(e) =>
-                e.target.checked &&
-                setFormData((prev) => ({ ...prev, role: "user" }))
-              }
+              defaultChecked={user && user.role === "user"}
             />
             <label className="flex-grow" htmlFor="user-role">
               User
@@ -95,12 +32,8 @@ export default function UserForm({ user, edit = false }) {
               name="role"
               id="admin-role"
               type="radio"
-              defaultChecked={formData.role === "admin"}
+              defaultChecked={user && user.role === "admin"}
               disabled={edit && product.role === "admin"}
-              onChange={(e) =>
-                e.target.checked &&
-                setFormData((prev) => ({ ...prev, role: "admin" }))
-              }
             />
             <label className="flex-grow" htmlFor="admin-role">
               Admin
@@ -114,8 +47,7 @@ export default function UserForm({ user, edit = false }) {
             name="name"
             type="text"
             placeholder="Full Name"
-            value={(formData && formData.name) || ""}
-            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+            defaultValue={(user && user.name) || ""}
             className="w-full px-2 py-3 placeholder:text-slate-500 focus-visible:outline-0 border border-slate-200 rounded-xl bg-white shadow-xs"
           />
         </div>
@@ -126,20 +58,18 @@ export default function UserForm({ user, edit = false }) {
             name="email"
             type="email"
             placeholder="Email address"
-            value={(formData && formData.email) || ""}
-            onChange={(e) =>
-              setFormData({ ...formData, email: e.target.value })
-            }
+            defaultValue={(user && user.email) || ""}
             className="w-full px-2 py-3 placeholder:text-slate-500 focus-visible:outline-0 border border-slate-200 rounded-xl bg-white shadow-xs"
           />
         </div>
 
         <div className="mb-3">
+          {/* Auto generate password */}
           <input
             required
             name="password"
-            type="text"
-            placeholder="Password: Auto Generated"
+            type="password"
+            placeholder="Auto Generated"
             disabled={true}
             className="w-full px-2 py-3 placeholder:text-slate-500 focus-visible:outline-0 border border-slate-200 rounded-xl bg-white shadow-xs"
           />
@@ -151,10 +81,7 @@ export default function UserForm({ user, edit = false }) {
             name="businessName"
             type="text"
             placeholder="Business Name"
-            value={(formData && formData.businessName) || ""}
-            onChange={(e) =>
-              setFormData({ ...formData, businessName: e.target.value })
-            }
+            defaultValue={(user && user.businessName) || ""}
             className="w-full px-2 py-3 placeholder:text-slate-500 focus-visible:outline-0 border border-slate-200 rounded-xl bg-white shadow-xs"
           />
         </div>
@@ -165,10 +92,7 @@ export default function UserForm({ user, edit = false }) {
             name="businessCode"
             type="text"
             placeholder="Business Code"
-            value={(formData && formData.businessCode) || ""}
-            onChange={(e) =>
-              setFormData({ ...formData, businessCode: e.target.value })
-            }
+            defaultValue={(user && user.businessCode) || ""}
             className="w-full px-2 py-3 placeholder:text-slate-500 focus-visible:outline-0 border border-slate-200 rounded-xl bg-white shadow-xs"
           />
         </div>
@@ -179,12 +103,7 @@ export default function UserForm({ user, edit = false }) {
               name="isActive"
               id="active"
               type="radio"
-              defaultChecked={formData.isActive === "active"}
-              disabled={edit && product.isActive === "active"}
-              onChange={(e) =>
-                e.target.checked &&
-                setFormData((prev) => ({ ...prev, isActive: "active" }))
-              }
+              defaultChecked={user && user.isActive === "active"}
             />
             <label className="flex-grow" htmlFor="active">
               Active
@@ -196,12 +115,7 @@ export default function UserForm({ user, edit = false }) {
               name="isActive"
               id="notActive"
               type="radio"
-              defaultChecked={formData.isActive === "notActive"}
-              disabled={edit && product.isActive === "notActive"}
-              onChange={(e) =>
-                e.target.checked &&
-                setFormData((prev) => ({ ...prev, isActive: "notActive" }))
-              }
+              defaultChecked={user && user.isActive === "notActive"}
             />
             <label className="flex-grow" htmlFor="notActive">
               Not Active
@@ -212,11 +126,10 @@ export default function UserForm({ user, edit = false }) {
         <div className="pt-4">
           <motion.button
             whileTap={{ scale: 0.9 }}
-            type="button"
-            onClick={handleSubmit}
-            className={`w-full px-3 py-2 ${loading ? "bg-slate-200 text-slate-700" : "bg-slate-700 text-white"} font-semibold rounded-xl`}
+            type="submit"
+            className={`w-full px-3 py-2 ${isPending ? "bg-slate-200 text-slate-700" : "bg-slate-700 text-white"} font-semibold rounded-xl`}
           >
-            {loading ? (
+            {isPending ? (
               edit ? (
                 <span>Saving...</span>
               ) : (
@@ -230,19 +143,9 @@ export default function UserForm({ user, edit = false }) {
           </motion.button>
         </div>
 
-        {error && error.length > 0 && (
+        {state.message && state.message.length > 0 && (
           <div className="mt-3">
-            {error.map((err, i) => (
-              <p key={`error-${i}`} className="text-red-600">
-                {err}
-              </p>
-            ))}
-          </div>
-        )}
-
-        {message && message.length > 0 && (
-          <div className="mt-3">
-            <p className="text-green-600">{message}</p>
+            <p className="text-green-600">{state.message}</p>
           </div>
         )}
       </form>
