e47672f122bf964d0bbe25fb42f4b0a10875e9b6
Dhavidy <p266dd@gmail.com>
2025-04-28 09:12:17 +0900

    Added logo and fixed nexts.js remote image loading.

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,14 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["firebasestorage.googleapis.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "/v0/b/ironclad-app.firebasestorage.app/**",
+      },
+    ],
   },
 };
 
